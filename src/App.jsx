62e2b398201e99a03f9d2fb518645fe8fa6b5c98
twani <jbestius@gmail.com>
2024-09-26 21:00:40 +0200

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-[#18181a] flex flex-col ">
+    <div className="h-screen w-screen bg-slate-900 flex flex-col">
       <h1 className="text-5xl text-slate-300 my-6 text-center">CSS Shape outside</h1>
-        <div>
-          <img src="/assets/images/orange.png" alt="" />
+        <div className='h-[200px] w-[200px] mx-auto'>
+          <img className='h-full w-full object-cover' src="/assets/images/orange.png" alt="" />
         </div>
     </div>
   )
