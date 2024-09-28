2f88c966082011f7bb001970db94563964bcdd1c
twani <jbestius@gmail.com>
2024-09-28 16:28:32 +0200

    fix: increase size of drop and intitial y translation

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div className="h-screen w-screen bg-slate-900 flex flex-col">
       <h1 className="text-5xl text-slate-300 my-6 text-center">CSS Shape outside</h1>
-        <div className='h-[200px] w-[200px] mx-auto relative'>
+        <div className='h-[300px] w-[300px] mx-auto relative'>
           <img className='h-full w-full object-cover relative z-[1]' src="/assets/images/orange.png" alt="" />
-          <div className='h-[30px] w-[30px] bg-[#fe8238] rounded-full flex justify-center ml-[100px] -mt-3 absolute z-[2] animate-[dropDown_3s_ease-in-out_infinite]'>
-            <span className='block h-[15px] w-[15px] bg-[#fe8238] -mt-[2px] rotate-45'></span>
+          <div className='h-[30px] w-[30px] bg-[#fe8238] rounded-full flex justify-center left-1/2 -mt-3 absolute z-[2] animate-[dropDown_3s_ease-in-out_infinite]'>
+            <span className='block h-[17px] w-[17px] bg-[#fe8238] -mt-[2px] rotate-45'></span>
           </div>
         </div>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
           "0%":{transform: "translateY(-20px)",
             opacity:"0.2"
           },
-          "20%":{transform: "translateY(10px)",
+          "60%":{transform: "translateY(4px)",
             opacity:"100"
           },
           "100%": {transform: "translateY(100vh)",
